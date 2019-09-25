8bcb0ada8a9720be2f4ed516aa9525e53e6a91f5
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-25 04:40:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Add your code here
+class Dog 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each do |doggo|
+      puts doggo
+    end
+  end
+end
