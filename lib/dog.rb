22d79ab5cafaa8f16c0c63c548f59ecbada8beef
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-25 04:50:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,10 +18,10 @@ class Dog
   end
   
   def self.print_all
-    # @@all.each do |doggo|
+    @@all.each do |doggo|
       binding.pry
-      # puts doggo
-    # end
+      puts doggo.name
+    end
   end
   
   def self.clear_all
